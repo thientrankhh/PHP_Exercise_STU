9ebc17431cbcd4efe998f43606777cef63242fe1
thientran <thientrankhh@gmail.com>
2020-04-03 16:50:54 +0700

    Push code ex1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Circle</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	</head>
+	<body class="container" style="width: 500px;">
+		<h1>Circle</h1>
+		<form action="xuly.php" method="POST" accept-charset="utf-8" class="form-group">
+			<label class="form-text">Superficies</label>
+			<input type="number" name="superficies" placeholder="Enter Superficies" class="form-control">
+			<label class="form-text">Radius</label>
+			<input type="number" name="radius" placeholder="Enter Radius" class="form-control">
+			<input type="submit" name="action" class="btn btn-primary" class="form-group">
+		</form>
+	</body>
+</html>
